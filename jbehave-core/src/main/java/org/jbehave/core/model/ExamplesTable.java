0a37d91fff5551bbac3d667d329a806436f10bc3
skyoker <dmitry.targonsky@gmail.com>
2019-04-23 18:30:06 +0300

    Add support of story-level ExamplesTable-s

--- jbehave-core/src/main/java/org/jbehave/core/model/ExamplesTable.java
+++ jbehave-core/src/main/java/org/jbehave/core/model/ExamplesTable.java
@@ -415,6 +415,10 @@ public class ExamplesTable {
         return format();
     }
 
+    public boolean isEmpty() {
+        return headers.isEmpty();
+    }
+
     public void outputTo(PrintStream output) {
         output.print(asString());
     }
